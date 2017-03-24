09dbaa87c4a851e7cdb98f142b8fe92cff103b81
jarden <372002582@qq.com>
2017-03-24 15:05:56 +0800

    update

---
+++ fontend-env-install.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+#--------------------------------------------
+# 使用HomeBrew来快速搭建 前端开发环境
+# author：jarden-liu
+# site：https://github.com/jarden-liu/awesome-script
+#--------------------------------------------
+
+
+#安装HomeBrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+#安装atom
+brew install Caskroom/cask/atom
+
+#安装搜狗输入法
+brew install Caskroom/cask/sogouinput
+
+#安装qq
+brew install Caskroom/cask/qq
+
+#安装qqbrowser
+brew install Caskroom/cask/qqbrowser
+
+#安装qqmusic
+brew install Caskroom/cask/qqmusic
+
+#安装谷歌浏览器
+brew install Caskroom/cask/google-chrome
+
+#安装git
+brew install git
+
+#安装node
+brew install node
+
+#安装迅雷
+brew install Caskroom/cask/thunder
+
+
+#安装synergy   键鼠共享软件
+brew install Caskroom/cask/synergy
+
+
+#安装shadowsocksx 搭梯子  嘿嘿嘿
+brew install Caskroom/cask/shadowsocksx-ng
+
+
+#安装sourcetree
+brew install Caskroom/cask/sourcetree
+
+
+#安装网易云音乐
+brew install Caskroom/cask/neteasemusic
+
+#安装迅雷
+brew install Caskroom/cask/thunder
---
+++ node-env-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo npm config set registry https://registry.npm.taobao.org
+
+#安装ionic cordova
+sudo npm install -g ionic cordova gulp bower vue-cli
+
+#安装betta
+sudo npm install -g https://github.com/jarden-liu/betta.git
